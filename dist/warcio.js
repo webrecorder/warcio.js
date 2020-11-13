e8d0f9e4e209b635ef973e62908c6ce89cd09434
Ilya Kreymer <ikreymer@gmail.com>
2020-11-12 23:19:46 -0800

    build: update dist/ for 1.3.0

--- dist/warcio.js
+++ dist/warcio.js
@@ -4050,6 +4050,10 @@ class WARCRecord extends BaseAsyncIterReader
       warcHeaders.headers.set("Content-Type", defaultRecordCT[type]);
     }
 
+    if (!reader) {
+      async function* emptyReader() {}      reader = emptyReader();
+    }
+
     const record = new WARCRecord({warcHeaders, reader});
 
     switch (type) {
