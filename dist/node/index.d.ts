dc363d9c4a46b748e5633d3528f5c4a9f274814b
Ilya Kreymer <ikreymer@gmail.com>
2024-08-17 19:03:55 -0700

    readd dist/ switch @types/pako version to 1.0.7 to match pako version

---
+++ dist/node/index.d.ts
@@ -0,0 +1,25 @@
+import { WriteStream } from 'node:fs';
+import { f as WARCSerializerOpts$1, b as WARCSerializer$1, W as WARCRecord, j as SerializerInMemBuffer } from '../warcserializer-48f5a2a4.js';
+import 'hash-wasm/dist/lib/WASMInterface';
+import 'pako';
+import '../types-af1fe7fc.js';
+
+declare const DEFAULT_MEM_SIZE: number;
+type WARCSerializerOpts = WARCSerializerOpts$1 & {
+    maxMemSize?: number;
+};
+declare class WARCSerializer extends WARCSerializer$1 {
+    static serialize(record: WARCRecord, opts?: WARCSerializerOpts): Promise<Uint8Array>;
+    constructor(record: WARCRecord, opts?: WARCSerializerOpts);
+}
+declare class TempFileBuffer extends SerializerInMemBuffer {
+    memSize: number;
+    currSize: number;
+    fh: WriteStream | null;
+    filename: string;
+    constructor(memSize?: number);
+    write(chunk: Uint8Array): void;
+    readAll(): AsyncIterable<Uint8Array>;
+}
+
+export { DEFAULT_MEM_SIZE, TempFileBuffer, WARCSerializer, WARCSerializerOpts };
---
+++ dist/utils.d.ts
@@ -0,0 +1,2 @@
+export { a as appendRequestQuery, d as concatChunks, g as getSurt, j as jsonToQueryParams, b as jsonToQueryString, m as mfdToQueryParams, c as mfdToQueryString, p as postToGetUrl, s as splitChunk } from './utils-cbe6a7fc.js';
+import './types-af1fe7fc.js';
