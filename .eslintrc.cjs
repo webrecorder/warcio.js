4be185ebba214336f841c3f99de9506b8052ad44
emma <hi@emma.cafe>
2024-08-19 19:59:16 -0400

    add script to build if not in git repo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,4 +25,13 @@ module.exports = {
     semi: ["error", "always"],
     "deprecation/deprecation": "warn",
   },
+  overrides: [
+    {
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
+      files: ["webpack.*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
 };
